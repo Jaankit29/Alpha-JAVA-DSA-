dfc38012867a6fa33cacd2e744f22d9ed478eacf
Ankit Jakhania <86586431+Jaankit29@users.noreply.github.com>
2024-03-29 04:00:40 +0000

    change

--- 2D array/inrto.java
+++ 2D array/inrto.java
@@ -1,10 +1,24 @@
 /**
  * inrto
  */
+import java.util.*;
 public class inrto {
 
     public static void main(String[] args) {
         System.out.println("working");
-        System.out.println(3*7);
+        int matrix[][]= new int[3][3];
+
+        Scanner sc= new Scanner(System.in);
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+matrix[i][j]= sc.nextInt();
+            }
+        }
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+           System.out.print(matrix[i][j]+ " ");
+            }
+            System.out.println();
+        }
     }
 }
